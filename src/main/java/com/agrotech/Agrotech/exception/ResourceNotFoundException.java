c0344fd5473c689f0ddafa62b9cf6635313033a0
mateo-stay <mate.estay@duocuc.cl>
2025-06-07 23:20:13 -0400

    cambios

--- src/main/java/com/agrotech/Agrotech/exception/ResourceNotFoundException.java
+++ src/main/java/com/agrotech/Agrotech/exception/ResourceNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String resourceName, Long id) {
-        super(String.format("%s con id %d no encontrado", resourceName, id));
+        super("%s con id %d no encontrado".formatted(resourceName, id));
     }
 }
