a1cdaad4dfab093dee340cd2d46eef491603b67e
mateo-stay <mate.estay@duocuc.cl>
2025-06-08 20:53:57 -0400

    Cambios

---
+++ src/main/java/com/agrotech/AgrotechApplication.java
@@ -0,0 +1,13 @@
+package com.agrotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrotechApplication.class, args);
+    }
+
+}
