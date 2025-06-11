7281235c8653707cfe13e1a6eb2124ef2189797a
Heyitsjorry <jor.contreras@duocuc.cl>
2025-06-10 22:28:47 -0400

    cambios Rama Jordana

--- src/main/java/com/agrotech/Agrotech/model/SensorData.java
+++ src/main/java/com/agrotech/Agrotech/model/SensorData.java
@@ -1,10 +1,12 @@
 package com.agrotech.Agrotech.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.;
+import jakarta.validation.constraints.;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -14,17 +16,43 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class SensorData {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Double temperatura;          // °C
+    @NotNull(message = "La temperatura es obligatoria")
+    @DecimalMin(value = "0.0", message = "La temperatura no puede ser negativa")
+    private Double temperatura;
+
+    @NotBlank(message = "La unidad de temperatura es obligatoria")
     private String unidadTemperatura;
-    private Double humedad;              // %
+
+    @NotNull(message = "La humedad es obligatoria")
+    @DecimalMin(value = "0.0", message = "La humedad no puede ser menor a 0")
+    @DecimalMax(value = "100.0", message = "La humedad no puede ser mayor a 100")
+    private Double humedad;
+
+    @NotBlank(message = "La unidad de humedad es obligatoria")
     private String unidadHumedad;
-    private Integer nivelAgua;           // valor crudo (0-4095)
-    private Double distanciaUltrasonica; // cm
+
+    @NotNull(message = "El nivel de agua es obligatorio")
+    @Min(value = 0, message = "Nivel de agua no puede ser menor a 0")
+    @Max(value = 4095, message = "Nivel de agua no puede ser mayor a 4095")
+    private Integer nivelAgua;
+
+    @NotNull(message = "La distancia ultrasonica es obligatoria")
+    @DecimalMin(value = "0.0", message = "La distancia no puede ser negativa")
+    private Double distanciaUltrasonica;
+
+    @NotBlank(message = "La unidad de distancia es obligatoria")
     private String unidadDistancia;
 
+    @PastOrPresent(message = "La fecha no puede ser futura")
+    @Column(updatable = false)
+    @CreationTimestamp
     private LocalDateTime timestamp;
+
+    private String origenSensor; 
+    private String tipoSensor;
 }
